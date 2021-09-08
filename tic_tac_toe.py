fac651a489134bffa06f08dfacdd8031671e88b1
brianpompey <brianthescholar@gmail.com>
2021-09-07 22:37:03 -0400

    Added fix spot

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,3 +16,14 @@ class TicTacToe
 
     def get_random_first_player(self):
         return random.randint(0, 1)
+
+    def fix_spot(self, row, col, player):
+        self.board[row][col] = player
+
+    def is_player_win(self, player):
+        win = None
+
+        n = len(self.board)
+
+        for i in range(n):
+            
